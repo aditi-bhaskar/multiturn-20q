34e360ca5ad9b62f6dd7448fc772d73333cf6379
Aditi <aditijb@cs.stanford.edu>
2025-05-20 09:12:24 -0700

    ready to run some to generate actual json files

--- collab-llm/scripts/generate_conv_dpo_20q.sh
+++ collab-llm/scripts/generate_conv_dpo_20q.sh
@@ -19,21 +19,20 @@ export N_EVAL=100                   # aditi edit for debugging
 
 
 
-
-
 # debug time:
-export MAX_NUM_CONV=1           # number of objects/games
-export MAX_NEW_TURNS=2          # Maximum number of new conversation turns per task
-export MAX_NUM_WORKERS=1
+# export MAX_NUM_CONV=2           # number of objects/games
+# export MAX_NEW_TURNS=2          # Maximum number of new conversation turns per task
+# export MAX_NUM_WORKERS=1
 
 # obsolete?!:
 # export MAX_NUM_CONV=1000        # number of objects/games
 # export MAX_NUM_CONV=500           # number of objects/games -- WAY TOO MANY!! :(
 
 # test time:
-# export MAX_NUM_CONV=50           # number of objects/games
-# export MAX_NEW_TURNS=20           # Maximum number of new conversation turns per task
-# export MAX_NUM_WORKERS=4
+# export MAX_NUM_CONV=50           # number of objects/games -- just generate 10 at a time, maybe
+export MAX_NUM_CONV=10           # number of objects/games
+export MAX_NEW_TURNS=20           # Maximum number of new conversation turns per task
+export MAX_NUM_WORKERS=4
 
 
 
