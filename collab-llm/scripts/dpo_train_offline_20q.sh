0d23ba4f9fb69b264f22bd569307c1fa3cc26579
Aditi <aditijb@cs.stanford.edu>
2025-05-22 22:41:46 -0700

    checkpoint before i swap to using linux

--- collab-llm/scripts/dpo_train_offline_20q.sh
+++ collab-llm/scripts/dpo_train_offline_20q.sh
@@ -5,6 +5,10 @@
 # Usage: ./scripts/eval_multiturn.sh  aditijb/collabllm-20q
 # source scripts/config.sh
 
+#  to make sure i can handle mps operations using my cpu if mps is not available
+export PYTORCH_ENABLE_MPS_FALLBACK=1
+
+
 export USE_SUB=false
 export USE_SNAP=true
 export USE_GCR=false
@@ -42,10 +46,13 @@ DATASET=aditijb/collabllm-20q  # hardcode the dataset we use for training
 # CUDA_VISIBLE_DEVICES=0,1,2,3,5,6,7 torchrun --master_port=$PORT --nnodes=1 --nproc_per_node=7 \
 # torchrun --master_port=$PORT --nnodes=1 --nproc_per_node=7 \
 
-CUDA_VISIBLE_DEVICES=4 \
-    torchrun --master_port=$PORT \
-    --nnodes=1 --nproc_per_node=1 \
-     scripts/dpo_train_offline_20q.py \
+# CUDA_VISIBLE_DEVICES=4 \
+
+# torchrun --master_port=$PORT \
+# --nnodes=1 --nproc_per_node=1 \
+
+python \
+scripts/dpo_train_offline_20q.py \
     --datasets $DATASET \
     --assistant_model_name $ASSISTANT_MODEL_NAME \
     --gradient_accumulation_steps 8 \
